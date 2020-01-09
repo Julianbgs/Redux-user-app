020b37f8689253735680e258f95fdda2f00d7d78
Julian Blagis <julianbgs96@gmail.com>
2020-01-09 16:35:08 +0300

    refactor code and styled components

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -1,11 +1,12 @@
 export const ADD_USER = 'ADD_USER';
 
-export function addUser(name, age) {
+export function addUser(name, age, id) {
   return {
     type: ADD_USER,
     payload: {
       name,
-      age
+      age,
+      id
     }
   }
 }
--- src/components/Form/Index.jsx
+++ src/components/Form/Index.jsx
@@ -6,17 +6,26 @@ import './Form.sass';
 
 class FormUsers extends Component {
 
+  constructor(props) {
+    super(props);
+    this.nameRef = React.createRef();
+    this.ageRef = React.createRef();
+  }
+
   handleUserAdd = (e) => {
     e.preventDefault();
-    this.props.addUser('test', 23);
+    const name = this.nameRef.current.value;
+    const age = this.ageRef.current.value;
+    const id  = Math.random() * 10;
+    this.props.addUser(name, age, id);
   };
 
   render() {
     return (
       <div className="Form" onSubmit={this.handleUserAdd}>
         <form className="Form__Wrap">
-          <input type="text" className="Form__Name" />
-          <input type="text" className="Form__Age" />
+          <input type="text" className="Form__Input" ref={this.nameRef} />
+          <input type="text" className="Form__Input" ref={this.ageRef} />
           <input type="submit" className="Form__Submit" />
         </form>
       </div>
